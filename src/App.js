4d98e8e55efc39c890484e6892e50b792fcc2654
PerceptSystems <raj@perceptsystems.com>
2025-10-16 12:18:19 +0530

    Salesforce sdk setup

--- src/App.js
+++ src/App.js
@@ -7,33 +7,6 @@ import EmployeeProfile from "./pages/EmployeeProfile";
 import OrgProfile from "./pages/OrgProfile";
 
 function App() {
-  useEffect(() => {
-    window.Sfdc.canvas.onReady(function () {
-      console.log("Salesforce sdk init.....");
-      // Extract signed request
-      const sr = JSON.parse(window.name.split("=")[1]);
-      console.log("Signed Request:", sr);
-
-      // Extract OAuth token & instance URL
-      const { client, context } = sr;
-      const { instance_url, oauthToken } = client;
-      const { user, organization } = context;
-
-      console.log("Org ID:", organization.orgId);
-      console.log("User ID:", user.userId);
-
-      // Example: Fetch Salesforce users
-      fetch(
-        `${instance_url}/services/data/v61.0/query?q=SELECT+Id,Name+FROM+User+LIMIT+10`,
-        {
-          headers: { Authorization: `Bearer ${oauthToken}` },
-        }
-      )
-        .then((r) => r.json())
-        .then((data) => console.log(data));
-    });
-  }, []);
-
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-r from-green-950 to-green-200 p-6 text-white">
